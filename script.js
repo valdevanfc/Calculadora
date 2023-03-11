f3532076399aed87793bf057c3e3bea15077b538
valdevanfc <123906067+valdevanfc@users.noreply.github.com>
2023-03-11 15:42:52 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function inserirValor(numero) {
+    const num = document.getElementById('caixa_resultado').innerHTML;
+    document.getElementById('caixa_resultado').innerHTML = num + numero;
+}
+
+function apagar_resultado() {
+    document.getElementById('caixa_resultado').innerHTML = "";
+}
+
+function apagar() {
+    const result = document.getElementById('caixa_resultado').innerHTML;
+    document.getElementById('caixa_resultado').innerHTML = result.substring(0, result.length - 1);
+}
+
+function efetuarIgualdade() {
+    const valor = document.getElementById('caixa_resultado').innerHTML;
+    if (valor) {
+        document.getElementById('caixa_resultado').innerHTML = eval(valor);
+    }
+}
